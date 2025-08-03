88d2781804a28e8599f24d2674c020e7a2704e62
Timur Gaifulin <gaifulin.tr@gmail.com>
2025-08-03 14:00:19 +0300

    Add check_function for SODA quality checks

--- dags/retail.py
+++ dags/retail.py
@@ -46,4 +46,12 @@ def retail():
         ),
         use_native_support=False,
     )
+
+    @task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
+    def check_load(sacn_name='check_load', checks_subpath='sources'):
+        from include.soda.check_function import check
+
+        return check(scan_name, checks_subpath)
+
+    check_load()
 retail()
